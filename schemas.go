7f480f30a7b650e7fd75deceba65d6b3151bd841
James Greenhill <fuziontech@gmail.com>
2024-06-28 16:55:58 -0700

    feat: add schema dump, but in order

--- schemas.go
+++ schemas.go
@@ -1,51 +1,173 @@
 package main
 
 import (
-	"context"
+	"database/sql"
 	"fmt"
-	"log"
-
-	"github.com/ClickHouse/clickhouse-go/v2"
-	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+	"os"
 )
 
-func dumpTableSchema(ctx context.Context, conn driver.Conn, database, table string) {
-	rows, err := conn.Query(
-		ctx,
-		"SHOW CREATE TABLE {database:Identifier}.{table:Identifier};",
-		clickhouse.Named("database", database),
-		clickhouse.Named("table", table))
-	if err != nil {
-		log.Fatal(err)
-	}
-	for rows.Next() {
-		var (
-			row string
-		)
-		if err := rows.Scan(
-			&row,
-		); err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("\n/* CREATE TABLE STATEMENT FOR TABLE:", table, " */")
-		fmt.Println(row)
-	}
+type Options struct {
+	DB          *sql.DB
+	Path        string
+	SpecifiedDB string
 }
 
-func dumpSchema(ctx context.Context, conn driver.Conn, database string) {
-	rows, err := conn.Query(ctx, "select name from system.tables where database = {database:String} group by name order by name;", clickhouse.Named("database", database))
-	if err != nil {
-		log.Fatal(err)
+var (
+	tableEngines = []string{
+		"%MergeTree",
+		"Kafka",
+		"Distributed",
+		"Dictionary",
+		"Join",
+		"MaterializedView",
+		"View",
 	}
-	for rows.Next() {
-		var (
-			tableName string
-		)
-		if err := rows.Scan(
-			&tableName,
-		); err != nil {
-			log.Fatal(err)
+)
+
+func Write(opts *Options) error {
+	var fd *os.File
+	var err error
+	if len(opts.Path) > 0 {
+		fd, err = os.OpenFile(opts.Path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+		if err != nil {
+			return fmt.Errorf("opening file: %v", err)
 		}
-		dumpTableSchema(ctx, conn, database, tableName)
+		defer fd.Close()
+	} else {
+		fd = os.Stdout
 	}
+
+	allDatabases, err := getDatabases(opts.DB)
+	if err != nil {
+		return fmt.Errorf("getting databases: %v", err)
+	}
+
+	var databases []string
+	switch opts.SpecifiedDB {
+	case "":
+		databases = allDatabases
+	case "system":
+		return fmt.Errorf("'%s' is a special internal ClickHouse database and can't be specified", opts.SpecifiedDB)
+	default:
+		if includes(allDatabases, opts.SpecifiedDB) {
+			databases = []string{opts.SpecifiedDB}
+		} else {
+			return fmt.Errorf("specified database '%s' doesnt exist", opts.SpecifiedDB)
+		}
+	}
+
+	for _, dbName := range databases {
+		if dbName == "system" {
+			continue
+		}
+		dbCreateStmt, err := dbCreateStmt(opts.DB, dbName)
+		if err != nil {
+			return err
+		}
+		_, err = fd.Write([]byte(dbCreateStmt + "\n\n"))
+		if err != nil {
+			return fmt.Errorf("writing database '%s' create statement: %v", dbName, err)
+		}
+
+		var tables []string
+		for _, engine := range tableEngines {
+			newTables, err := getTables(opts.DB, dbName, engine)
+			if err != nil {
+				return err
+			}
+			tables = append(tables, newTables...)
+		}
+		for _, tableName := range tables {
+			tableCreateStmt, err := fetchTableCreateStmt(opts.DB, dbName, tableName)
+			if err != nil {
+				return err
+			}
+			_, err = fd.Write([]byte(tableCreateStmt + "\n\n"))
+			if err != nil {
+				return fmt.Errorf("writing table '%s' create statement: %v", tableName, err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func getDatabases(db *sql.DB) ([]string, error) {
+	var databases []string
+	rows, err := db.Query("SHOW DATABASES FORMAT TabSeparated;")
+	if err != nil {
+		return []string{}, fmt.Errorf("getting databases: %v", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var name string
+		if err := rows.Scan(&name); err != nil {
+			return []string{}, fmt.Errorf("getting databases: %v", err)
+		}
+		databases = append(databases, name)
+	}
+
+	if rows.Err(); err != nil {
+		return []string{}, fmt.Errorf("getting databases: %v", err)
+	}
+
+	return databases, nil
+}
+
+func getTables(db *sql.DB, dbName string, engineFilter string) ([]string, error) {
+	var tables []string
+	rows, err := db.Query("SELECT name FROM system.tables WHERE database = ? and engine like ?;", dbName, engineFilter)
+	if err != nil {
+		return []string{}, fmt.Errorf("getting tables for '%s': %v", dbName, err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var name string
+		if err := rows.Scan(&name); err != nil {
+			return []string{}, fmt.Errorf("getting tables for '%s': %v", dbName, err)
+		}
+		if !(len(name) > 6 && name[:7] == ".inner.") {
+			tables = append(tables, name)
+		}
+	}
+
+	if rows.Err(); err != nil {
+		return []string{}, fmt.Errorf("getting tables for '%s': %v", dbName, err)
+	}
+
+	return tables, nil
+}
+
+func dbCreateStmt(db *sql.DB, dbName string) (string, error) {
+	var createStmt string
+	queryStmt := fmt.Sprintf("SHOW CREATE DATABASE %s FORMAT PrettySpaceNoEscapes;", dbName)
+	err := db.QueryRow(queryStmt).Scan(&createStmt)
+	if err != nil {
+		return "", fmt.Errorf("getting database %s create statement: %v", dbName, err)
+	}
+
+	return createStmt, nil
+}
+
+func fetchTableCreateStmt(db *sql.DB, dbName string, tableName string) (string, error) {
+	var createStmt string
+	queryStmt := fmt.Sprintf("SHOW CREATE TABLE `%s`.`%s` FORMAT PrettySpaceNoEscapes;", dbName, tableName)
+	err := db.QueryRow(queryStmt).Scan(&createStmt)
+	if err != nil {
+		return "", fmt.Errorf("getting table '%s.%s' statement: %v", dbName, tableName, err)
+	}
+
+	return createStmt, nil
+}
+
+func includes(strs []string, str string) bool {
+	for _, s := range strs {
+		if s == str {
+			return true
+		}
+	}
+
+	return false
 }

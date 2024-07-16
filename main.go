84417dad1c5f7863400541d7e932ccb47f6cb399
James Greenhill <fuziontech@gmail.com>
2024-07-16 16:51:40 -0700

    feat: add a compare-schemas command that compares tables on two different clusters

--- main.go
+++ main.go
@@ -125,6 +125,45 @@ func main() {
 	cmd.AddCommand(dumpSchemaCmd)
 
 	cmd.AddCommand(&cobra.Command{
+		Use:   "compare-schema",
+		Short: "compare schemas from <clickhouse_url> to <clickhouse_url> <database> ",
+		Args:  cobra.MinimumNArgs(3),
+		Run: func(cmd *cobra.Command, args []string) {
+			var (
+				clickhouseUrl  = &args[0]
+				clickhouse2Url = &args[1]
+				specifiedDB    = &args[2]
+			)
+
+			conn, err := NewCHConn(clickhouseUrl)
+			if err != nil {
+				fmt.Printf("Error connecting to the database: %v\n", err)
+				os.Exit(1)
+			}
+			defer conn.Close()
+
+			conn2, err := NewCHConn(clickhouse2Url)
+			if err != nil {
+				fmt.Printf("Error connecting to the database: %v\n", err)
+				os.Exit(1)
+			}
+			defer conn2.Close()
+
+			opts := Options{
+				DB:          conn,
+				DB2:         conn2,
+				SpecifiedDB: *specifiedDB,
+			}
+
+			err = Compare(&opts)
+			if err != nil {
+				fmt.Printf("Error comparing schemas: %v\n", err)
+				os.Exit(1)
+			}
+		},
+	})
+
+	cmd.AddCommand(&cobra.Command{
 		Use:   "synctable",
 		Short: "subcommand to sync a table across clusters",
 		Args:  cobra.MinimumNArgs(1),
